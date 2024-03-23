16aa2311133a3be0a1e91f65f8ef67b6884746ac
RensKoster03 <164334276+RensKoster03@users.noreply.github.com>
2024-03-23 11:32:57 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,48 +1,71 @@
-let goblin = document.querySelector('#goblin');
+const goblin = document.querySelector('#goblin');
+let wizard = document.querySelector(".wizard")
 
 const scoreBoard = document.querySelector("#score");
-
 let score = 100;
 
 let timeLeft = 70;
-
 const timerInterval = setInterval(updateTimer, 1000);
 
 const grunt = new Audio ('assets/grunt.mp3');
-
-const fire = new Audio ('assets/fireball.mp3');
-
-const wizardNoise = ["assets/fireball.mp3", "assets/huah.mp3", "assets/kablamo.mp3", "assets/hiyah.mp3", "assets/huzzah.mp3", "assets/lightning_bolt"]
-
+const wizardNoise = ["assets/fireball.mp3", "assets/huah.mp3", "assets/kablamo.mp3", "assets/hiyah.mp3", "assets/huzzah.mp3", "assets/lightning_bolt.mp3"]
+// randomizer van wizardgeluiden
 function wizardSound(){
   const audioIndex = Math.floor(Math.random() * wizardNoise.length);
   const audio = new Audio(wizardNoise[audioIndex]);
   audio.play();
 }
-
+// timer 
 function updateTimer() {
   document.getElementById('timer').innerText = timeLeft;
   timeLeft--;
-
   if (timeLeft < 0) {
     clearInterval(timerInterval);
     window.location.href = 'gameover.html';
   }
 }
-
-function hitGoblin(){
+// updaten van hp 
+function scoreGoblin(){
     score = score - 1;
     scoreBoard.textContent = score;
-    goblin.style.left = Math.floor(Math.random() * 1200) + "px"
-    goblin.style.top = Math.floor(Math.random() * 300) + "px"
-    grunt.play()
-    wizardSound()
     if (score <= 0) {
       window.location.href = 'win.html';
     }
-
+    if (score <= 60) {
+      goblin.src = 'assets/goblinhurt1.png'
+    }
+    if (score <= 20) {
+      goblin.src = 'assets/goblinhurt2.png'
+    }
+}
+// Wizard animatie
+function changeWiz() {
+  wizard.src = 'assets/wizard_cast.png'
+  setTimeout(() => {
+    wizard.src = 'assets/thewizard.png'
+  }, 300);
+}
+// verplaatsen van goblin over de map
+function tpGoblin() {
+  goblin.style.left = Math.floor(Math.random() * 1200) + "px"
+  goblin.style.top = Math.floor(Math.random() * 200) + "px"
+}
+// geluid bij klikken
+function hitAudio() {
+  grunt.play()
+  wizardSound()
 }
 
-goblin.addEventListener('click', hitGoblin);
+goblin.addEventListener('click', scoreGoblin);
 
+goblin.addEventListener('click', tpGoblin)
 
+goblin.addEventListener('click', hitAudio)
+
+goblin.addEventListener('click', changeWiz)
+
+// Bronnen gebruikt:
+// https://pngimg.com/image/83360
+// https://copilot.microsoft.com
+// https://www.youtube.com/watch?v=icU0IPfv_-o
+// https://www.youtube.com/watch?v=-9EF4gDvfto
